bf84c8f69c58dd3af4724c89b34ba60e26c4be4c
Muhammad Zahalqa <m@tryfinally.com>
2023-12-01 02:53:24 +0200

    menu selection infra

---
+++ satellite-subsystems/demo/src/Demos/menu_selection.h
@@ -0,0 +1,34 @@
+/*
+ * menu_selection.h
+ *
+ *  Created on: Dec 1, 2023
+ *      Author: tryfinally
+ */
+
+#ifndef DEMOS_MENU_SELECTION_H_
+#define DEMOS_MENU_SELECTION_H_
+
+#include <hal/boolean.h>
+
+Boolean quit_menu();
+typedef Boolean (*MenuActionPtr)(void);
+
+typedef struct MenuAction {
+	MenuActionPtr action;
+	char const*  menuSelection;
+} MenuAction;
+
+#define RETURN_TO_PREVIOUS_MENU		{quit_menu, "Return to previous menu"}
+#define ARRAY_SIZE(a) 	(sizeof(a)/sizeof((a)[0]))
+
+/**
+ * Display menu and execute action selected
+ * @param tests array of menu item {function, description }
+ * @param size  number of elements in the array
+ * @return
+ */
+Boolean MenuSelectAndExecute(MenuAction tests[], unsigned int size);
+
+Boolean MenuDisplay(MenuAction tests[], unsigned int size);
+
+#endif /* DEMOS_MENU_SELECTION_H_ */
