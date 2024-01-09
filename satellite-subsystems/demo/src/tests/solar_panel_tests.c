e6690eafbbdd61789aaf8b1b465db78b9b2d711b
Muhammad Zahalqa <m@tryfinally.com>
2024-01-09 16:50:15 +0200

    Implement Solar Panel Sensor Tests

--- satellite-subsystems/demo/src/tests/solar_panel_tests.c
+++ satellite-subsystems/demo/src/tests/solar_panel_tests.c
@@ -6,26 +6,42 @@
  */
 
 #include "solar_panel_tests.h"
-
 #include "utils/menu_selection.h"
+
+#include "modules/m_solar_panels.h"
 
 Boolean get_solar_panels_sensor_state(void)
 {
+	printf("Get Solar Temperature Sensor State\n");
+	Boolean awake = solar_panels_is_awake();
+	printf("Solar Panel Sensor is %s awake.\n", awake ? "" : "not");
 	return TRUE;
+
 }
 
 Boolean activate_solar_panels_sensor(void)
 {
+	printf("Activate Solar Temperature Sensor\n");
+	solar_panels_wakeup();
 	return TRUE;
+
 }
 
 Boolean shutdown_solar_panels_sensor(void)
 {
+	printf("Shutdown Solar Temperature Sensor\n");
+	solar_panels_sleep();
 	return TRUE;
 }
 
 Boolean get_solar_panels_temperature(void)
 {
+	printf("Solar Panels Temperatures:\n");
+	float* values = solar_panels_get_temperature();
+
+	for (int i = 0; i < ISIS_SOLAR_PANEL_COUNT; ++i) {
+		printf("Panel[%d] temperature= %f\n", i, values[i]);
+	}
 	return TRUE;
 }
 
