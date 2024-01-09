5f1fbc8c25ae5f8fda6954c96ea20702d31ae6a4
Muhammad Zahalqa <m@tryfinally.com>
2024-01-09 18:57:28 +0200

    use enum for I2C addresse values

--- satellite-subsystems/demo/src/config/i2c_address.h
+++ satellite-subsystems/demo/src/config/i2c_address.h
@@ -8,11 +8,17 @@
 #ifndef I2C_ADDRESS_H_
 #define I2C_ADDRESS_H_
 
-#define GOM_EPS_ADDRESS			0x02	///< I2C address of GOMSpace EPS
-#define ANTENNA_A_ADDRESS		0x31	///< I2C address of the ANTENNA Side A
-#define ANTENNA_B_ADDRESS		0x32	///< I2C address of the ANTENNA Side B
+enum {
+	I2C_SPEED	= 100000
+};
 
-#define TRXVU_RC_ADDRESS 		0x60	///< I2C address of the VU_RC
-#define TRXVU_TC_ADDRESS			0x61	///< I2C address of the VU_TC
+
+enum {
+	GOM_EPS_ADDRESS	  =		0x02,	///< I2C address of GOMSpace EPS
+	ANTENNA_A_ADDRESS =		0x31,	///< I2C address of the ANTENNA Side A
+	ANTENNA_B_ADDRESS =		0x32,	///< I2C address of the ANTENNA Side B
+	TRXVU_RC_ADDRESS  =		0x60,	///< I2C address of the VU_RC
+	TRXVU_TC_ADDRESS  =		0x61,	///< I2C address of the VU_TC
+};
 
 #endif /* I2C_ADDRESS_H_ */
